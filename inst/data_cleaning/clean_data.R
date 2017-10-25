3bbcbaecb33d44e2b51f13a1c9a4915b3fe73c59
laderast <roshioshi@gmail.com>
2017-10-25 10:18:33 -0700

    added diabetes NHANES data

---
+++ inst/data_cleaning/clean_data.R
@@ -0,0 +1,17 @@
+##Script for cleaning NHANES data
+library(NHANES)
+library(tidyverse)
+data("NHANES")
+
+##Build Diabetes dataset
+##show there is an association between BMI and diabetes
+NHANES %>% select(BMI, Diabetes, Gender) %>% ggplot(aes(x=Diabetes, y=BMI)) + geom_boxplot() + facet_wrap(~Gender)
+
+##Bin Data into High (> 30) and Low BMI (<=30)
+bmi_diabetes <- NHANES %>% select(BMI,Diabetes, Gender) %>%
+  mutate(BMIstatus=ifelse(BMI > 30, "High", "Low"), BMIstatus=as.factor(BMIstatus))
+
+bmi_diabetes %>% ggplot(aes(x=Diabetes, fill=BMIstatus)) +
+  geom_bar(position="fill", color="black") + facet_wrap(~Gender)
+
+devtools::use_data(bmi_diabetes)
