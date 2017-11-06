5d61586a92ed049f52e43a041b90df3c5bc27363
laderast <roshioshi@gmail.com>
2017-11-06 10:28:45 -0800

    update of categorical data

--- inst/data_cleaning/clean_data.R
+++ inst/data_cleaning/clean_data.R
@@ -11,12 +11,16 @@ NHANES %>% select(BMI, Diabetes, Gender) %>% ggplot(aes(x=Diabetes, y=BMI)) + ge
 bmi_diabetes <- NHANES %>% dplyr::select(BMI,Diabetes, Gender, Age) %>% filter(Age > 20) %>%
   mutate(BMIstatus=ifelse(BMI > 30, "High", "Low"), BMIstatus=as.factor(BMIstatus))
 
+bmi_diabetes_raw <- bmi_diabetes
+
 bmi_diabetes <- droplevels(bmi_diabetes)
+
 
 bmi_diabetes %>% ggplot(aes(x=Diabetes, fill=BMIstatus)) +
   geom_bar(position="fill", color="black") + facet_wrap(~Gender)
 
 devtools::use_data(bmi_diabetes, overwrite=TRUE)
+devtools::use_data(bmi_diabetes_raw, overwrite=TRUE)
 
 ##Whickham dataset
 library(mosaic)
